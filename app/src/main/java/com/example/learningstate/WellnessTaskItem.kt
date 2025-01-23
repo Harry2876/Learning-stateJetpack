7d378ac8c34079db029e77fac5e1346b3705437b
ʜᴀʀɪᴏᴍ ʜᴀʀꜱʜ <acbcharryxyz@gmail.com>
2025-01-24 01:52:45 +0530

    Add Tasks List Data Layer

--- app/src/main/java/com/example/learningstate/WellnessTaskItem.kt
+++ app/src/main/java/com/example/learningstate/WellnessTaskItem.kt
@@ -4,23 +4,46 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WellnessTaskItem(taskName : String, onClose: () -> Unit, modifier: Modifier = Modifier) {
+fun WellnessTaskItem(taskName : String,
+                     onClose: () -> Unit,
+                     modifier: Modifier = Modifier,
+                     checked:Boolean,
+                     onCheckChange: (Boolean) -> Unit) {
     Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
         Text(
             modifier = Modifier.weight(1f).padding(start = 16.dp),
             text = taskName
         )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckChange
+        )
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = "Close")
         }
     }
+}
+
+@Composable
+fun WellnessTaskItemStateful(taskName: String, modifier: Modifier=Modifier) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+
+    WellnessTaskItem(taskName, checked = checkedState,
+        onCheckChange = {newValue -> checkedState = newValue},
+        onClose = {}, modifier = modifier)
 }
